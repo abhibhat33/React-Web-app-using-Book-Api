7a6d03fd47fae46f8e0762db51ee0f9e3664baa5
abhishek <abhishek@tikkl.com>
2023-10-21 15:12:56 +0530

    Fixed spacing issues in BookDetail page

--- openlibrary/src/pages/BookDetail.jsx
+++ openlibrary/src/pages/BookDetail.jsx
@@ -4,6 +4,7 @@ import wretch from 'wretch';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/black-and-white.css';
 import { Grid, Column } from '@carbon/react';
+import Markdown from 'react-markdown';
 import PlaceholderImage from '../placeHolderUrl';
 import '../styles/BookDetail.scss';
 
@@ -17,10 +18,6 @@ export default function BookDetail(){
   const params = useParams();
   const { key, keyId } = params;
   const [data, setData] = useState(null);
-  // eslint-disable-next-line no-unused-vars
-  const [loading, setLoading] = useState(true);
-  // eslint-disable-next-line no-unused-vars
-  const [error, setError] = useState(null);
   const [authorData, setAuthorData] = useState([]);
 
   useEffect(() => {
@@ -29,11 +26,9 @@ export default function BookDetail(){
       .json()
       .then((bookData) => {
         setData(bookData);
-        setLoading(false);
       })
       .catch((e) => {
-        setError(`This is an HTTP error: The status is ${e.message}`);
-        setLoading(false);
+        console.error(`This is an HTTP error: The status is ${e.message}`);
       });
   }, [key, keyId]);
 
@@ -50,59 +45,121 @@ export default function BookDetail(){
   }, [data]);
 
   const bookTitle = data && data.title ? <h1 className="book-title">{data.title}</h1> : null;
-  const bookDescription =    data && data.description !== undefined ? (
-    <div className="Description">
-      <h4>Description</h4>
-      <p className="book-description">{processText(data.description.value || data.description)}</p>
-    </div>
-  ) : null;
-  const subjects =    data && data.subjects !== undefined ? (
-    <div className="Subjects">
-      <h4>Subjects</h4>
-      <p className="book-subjects">
-        {processText(
-          data.subjects.map((subject) => subject.replace(/\d\d\d\d-\d\d-\d\d/g, '')).join(', ')
-        )}
-      </p>
-    </div>
-  ) : null;
+  const bookDescription = data && data.description !== undefined
+    ? processText(data.description.value || data.description)
+    : null;
+  const subjects =    data && data.subjects !== undefined
+    ? data.subjects.map((subject) => subject.replace(/\d\d\d\d-\d\d-\d\d/g, '')).join(', ')
+    : null;
 
   const imageSrc = data && data.covers && data.covers.length > 0 ? `https://covers.openlibrary.org/b/id/${data.covers[0]}-M.jpg` : null;
+
   return (
-    <div>
-      <Grid>
-        <Column lg={14} md={7} sm={4} className="b">
-          <Grid>
-            <Column lg={4} md={2} sm={2} className="book-cover">
-              {imageSrc && (
-              <LazyLoadImage effect="opacity" src={imageSrc} alt="Book cover" height="auto" width="100%" placeholderSrc={<PlaceholderImage />} />
-              )}
-            </Column>
-            <Column lg={10} md={5} sm={4} className="book-details">
-              <div className="BookPage">
-                <div className="BookPage_info">
-                  <div className="title_book">{bookTitle}</div>
-                  <div className="author_book">
-                    {authorData.length > 0 && (
-                    <h3>
-                      by
-                      {' '}
-                      {authorData.join(', ')}
-                    </h3>
-                    )}
+    <Grid className="BookPage" fullWidth>
+      <Column lg={14} md={7} sm={4} className="b">
+        <Grid className="Book__">
+          <Column lg={4} md={2} sm={1} className="book-cover">
+            {imageSrc && (
+            <LazyLoadImage effect="opacity" src={imageSrc} className="Book-src" alt="Book cover" height="auto" width="100%" placeholderSrc={<PlaceholderImage />} />
+            )}
+          </Column>
+          <Column lg={10} md={5} sm={4} className="book-details">
+            <Grid className="BookPage" fullWidth>
+              <Column lg={10} md={4} sm={4} className="BookPage_title">
+                <h1>{bookTitle}</h1>
+              </Column>
+              <Column lg={10} md={4} sm={4} className="author_book">
+                {authorData.length > 0 && (
+                  <h3>
+                    by
+                    {' '}
+                    {authorData.join(', ')}
+                  </h3>
+                )}
+              </Column>
+              <Column lg={10} md={4} sm={4} className="book-subjects">
+                {subjects && (
+                  <div>
+                    <h4>Subjects</h4>
+                    <p>
+                      <Markdown>
+                        {subjects.length > 500 ? `${subjects.slice(0, 500)}...` : subjects}
+                      </Markdown>
+                    </p>
                   </div>
-                </div>
-
-                {subjects}
-
-                {bookDescription}
-
-              </div>
-            </Column>
-          </Grid>
-
-        </Column>
-      </Grid>
-    </div>
+                )}
+              </Column>
+              <Column lg={10} md={4} sm={4} className="Description">
+                {bookDescription && (
+                  <div>
+                    <h4>Description</h4>
+                    <p>
+                      <Markdown>
+                        {bookDescription && bookDescription.length > 500 ? `${bookDescription.slice(0, 500)}...` : bookDescription}
+                      </Markdown>
+                    </p>
+                  </div>
+                )}
+              </Column>
+            </Grid>
+          </Column>
+        </Grid>
+      </Column>
+    </Grid>
   );
 }
+
+//   return (
+//     <div>
+//       <Grid className="BookPage" fullWidth>
+//         <Column lg={14} md={7} sm={4} className="b">
+//           <Grid>
+//             <Column lg={4} md={2} sm={1} className="book-cover">
+//               {imageSrc && (
+//                 <LazyLoadImage effect="opacity" src={imageSrc}
+// alt="Book cover" height="auto" width="100%" placeholderSrc={<PlaceholderImage />} />
+//               )}
+//             </Column>
+//             <Column lg={10} md={5} sm={4} className="book-details">
+//               <div className="BookPage">
+//                 <div className="BookPage_info">
+//                   <div className="title_book">{bookTitle}</div>
+//                   <div className="author_book">
+//                     {authorData.length > 0 && (
+//                       <h3>
+//                         by
+//                         {' '}
+//                         {authorData.join(', ')}
+//                       </h3>
+//                     )}
+//                   </div>
+//                 </div>
+//                 {subjects && (
+//                   <div className="Subjects">
+//                     <h4>Subjects</h4>
+//                     <p className="book-subjects">
+//                       <Markdown>
+//                         {subjects.length > 500 ? `${subjects.slice(0, 500)}...` : subjects}
+//                       </Markdown>
+//                     </p>
+//                   </div>
+//                 )}
+//                 {bookDescription && (
+//                   <div className="Description">
+//                     <h4>Description</h4>
+//                     <p>
+//                       <Markdown>
+//                         {bookDescription && bookDescription.length > 500 ?
+// `${bookDescription.slice(0, 500)}...` : bookDescription}
+//                       </Markdown>
+//                     </p>
+//                   </div>
+//                 )}
+//               </div>
+//             </Column>
+//           </Grid>
+//         </Column>
+//       </Grid>
+//     </div>
+//   );
+// }
